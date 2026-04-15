e2e51c90a71133e68d3afd8ccf687a3478a8583c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-15 20:56:16 -0300

    Adicionando Apostila06-Exercicio

---
+++ Aula06-Exercicios/src/br/com/fiap/exercicio01/model/Funcionario.java
@@ -0,0 +1,9 @@
+package br.com.fiap.exercicio01.model;
+
+public class Funcionario {
+    private String nome;
+    private long matricula;
+    private double salario;
+
+    Profissao profissao = new Profissao();
+}
