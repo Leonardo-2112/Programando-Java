fe427888f4d68fb02033d5d19231a25c51622e7b
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-17 20:59:55 -0300

    Adicionando Apostila07

---
+++ Apostila07/src/br/com/fiap/apostila07/view/ExemploConversao.java
@@ -0,0 +1,18 @@
+package br.com.fiap.apostila07.view;
+
+import javax.swing.*;
+
+public class ExemploConversao {
+    public static void main(String[] args) {
+        String numero1 = JOptionPane.showInputDialog("Digite um número: "); // Lê como o scanner mas sempre retorna String
+        int n1 = Integer.parseInt(numero1);
+
+        //Ler mais um número
+        String numero2 = JOptionPane.showInputDialog("Digite outro número: ");
+        int n2 = Integer.parseInt(numero2);
+
+        //Exibir Soma
+        JOptionPane.showMessageDialog(null,"A soma é: " + (n1+n2)); //Exibe mensagem igual o sout mas com a caixinha na tela
+
+    }
+}
