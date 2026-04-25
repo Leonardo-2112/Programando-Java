94ef81d1bc8cee8c4b389ad999eb384ece41ad2a
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-25 16:02:22 -0300

    Alterando getters e setters ao screen-match

--- screen-match/src/br/com/alua/screenmatch/modelos/Filme.java
+++ screen-match/src/br/com/alua/screenmatch/modelos/Filme.java
@@ -9,53 +9,61 @@ public class Filme {
     private int duracaoEmMinutos;
 
     //Getters e Setters
+    //Nome
     public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
+        return this.nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    //Ano de Lançamento
+    public int getAnoDeLancamento() {
+        return this.anoDeLancamento;
+    }
+
     public void setAnoDeLancamento(int anoDeLancamento) {
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    //Incluido no Plano
+    public boolean getIncluidoNoPlano() {
+        return this.incluidoNoPlano;
     }
 
     public void setIncluidoNoPlano(boolean incluidoNoPlano) {
         this.incluidoNoPlano = incluidoNoPlano;
     }
 
+    //Duração em minutos
+    public int getDuracaoEmMinutos() {
+        return this.duracaoEmMinutos;
+    }
+
     public void setDuracaoEmMinutos(int duracaoEmMinutos) {
         this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
-    public void exibeFichaTecnica(){
+    //Total de avaliações
+    public int getTotalDeAvaliacoes() {
+        return this.totalDeAvaliacoes;
+    }
+
+    //Métodos
+    public void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
         System.out.println("Duração em minutos: " + duracaoEmMinutos);
         System.out.println("Incluído no plano: " + incluidoNoPlano);
     }
-    public void avaliacao(double nota){
+
+    public void avaliacao(double nota) {
         somaDasAvaliacoes = somaDasAvaliacoes + nota;
         totalDeAvaliacoes++;
     }
-    public double pegaMedia(){
-        return somaDasAvaliacoes/totalDeAvaliacoes;
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
