9d008eee2e69f2e4999761b5c545b5e9d113e4bf
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-09-23 19:52:27 -0300

    Adicionando controller post do condominio

--- segundo semestre/Apostila16/src/main/java/br/com/fiap/api/controller/CondominioController.java
+++ segundo semestre/Apostila16/src/main/java/br/com/fiap/api/controller/CondominioController.java
@@ -1,10 +1,33 @@
 package br.com.fiap.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.api.dao.CondominioDao;
+import br.com.fiap.api.model.Condominio;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.sql.SQLException;
 
 @RestController
+@RequestMapping("/condominios")
 public class CondominioController {
+
+    private CondominioDao dao;
+
+    public CondominioController(CondominioDao dao){
+        this.dao = dao;
+    }
+
+    @PostMapping
+    public ResponseEntity<Condominio>inserir(@RequestBody Condominio condominio, UriComponentsBuilder uriBuilder) throws SQLException {
+
+        dao.inserir(condominio);
+        URI uri = uriBuilder.path("/condominios/{id}").buildAndExpand(condominio.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(condominio);
+    }
 
     @GetMapping("churros")
     public String dizerOla(){
